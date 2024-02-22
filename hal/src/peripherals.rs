c4156bbd46ab33dbdb639173670a2ac0f0d1f6e3
Andelf <andelf@gmail.com>
2024-02-23 01:22:37 +0800

    chore: basic embassy peripheral template

---
+++ hal/src/peripherals.rs
@@ -0,0 +1,14 @@
+use crate::pac;
+
+// We need to export this in the hal for the drivers to use
+
+crate::peripherals! {
+    UART0 <= UART0,
+    UART1 <= UART1,
+    UART2 <= UART2,
+    UART3 <= UART3,
+    UART4 <= UART4,
+
+
+
+}
