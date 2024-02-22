a4581a32d19b6a43eb1a04f99ca871a1d6edbc86
Andelf <andelf@gmail.com>
2024-02-23 03:00:41 +0800

    feat: add boot src detect

--- hal/src/lib.rs
+++ hal/src/lib.rs
@@ -8,9 +8,36 @@ pub mod peripherals;
 pub mod uart;
 
 pub mod ddr;
+pub mod signature;
 
 pub mod rom_api {
     extern "C" {
-        pub fn p_rom_api_get_boot_src() -> u32;
+        fn p_rom_api_get_boot_src() -> u32;
+    }
+
+    #[derive(Debug)]
+    #[repr(u32)]
+    pub enum BootSrc {
+        SpiNand = 0,
+        SpiNor = 2,
+        Emmc = 3,
+        SdCard = 0xA0,
+        Usb = 0xA3,
+        Uart = 0xA5,
+        Unknown(u32),
+    }
+
+    pub fn get_boot_src() -> BootSrc {
+        unsafe {
+            match p_rom_api_get_boot_src() & 0xFF {
+                0 => BootSrc::SpiNand,
+                2 => BootSrc::SpiNor,
+                3 => BootSrc::Emmc,
+                0xA0 => BootSrc::SdCard,
+                0xA3 => BootSrc::Usb,
+                0xA5 => BootSrc::Uart,
+                v => BootSrc::Unknown(v),
+            }
+        }
     }
 }
