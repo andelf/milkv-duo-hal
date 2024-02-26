3783369d3afb4931823ec2773cd91f2b38a43a82
Andelf <andelf@gmail.com>
2024-02-26 21:41:12 +0800

    chore: rm unused code

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -13,24 +13,13 @@ const BANNER: &str = r#"
 | |  | | | |   <_____\ V /   | |_| | |_| | (_) |
 |_|  |_|_|_|_|\_\     \_/    |____/ \__,_|\___/ "#;
 
-#[no_mangle]
-pub unsafe extern "riscv-interrupt-s" fn default_start_trap() {
-    let stval = riscv::register::stvec::read().bits();
-    let scause = riscv::register::scause::read();
-    let sscratch = riscv::register::sscratch::read();
-
-    crate::println!("stval: {:#x}", stval);
-    crate::println!("scause: {:?}", scause.bits());
-    crate::println!("sscratch: {:#x}", sscratch);
-
-    loop {}
-}
-
 #[milkv_duo_riscv_rt::entry]
 fn main() -> ! {
+    hal::sbi::put_string(BANNER);
+    hal::sbi::put_string(" - 256M\n");
+
     hal::uart::Uart0::init();
 
-    println!("{}", BANNER);
     println!("Boot Src: {:?}", hal::rom_api::get_boot_src());
     let chip_info = hal::signature::read_chip_info();
     println!("Chip info: {:x?}", chip_info);
